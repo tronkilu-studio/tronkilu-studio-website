4f1ed2d2cf83a317e2e71003fe1058849046aa9a
Ergi Shkëlzeni <ergi@tronkilu.studio>
2026-09-21 00:02:22 +0200

    clean_draft1

--- assets/logos/logo1/Vector.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface VectorProps {
-  className?: string;
-  style?: React.CSSProperties;
-}
-export declare const Vector: React.FC<VectorProps>;
-export default Vector;
